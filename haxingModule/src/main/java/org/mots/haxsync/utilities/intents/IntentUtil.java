ea0a5dbf19be129d22f869c5e6ad976ddd16866d
tishun <tihomir.mateev@gmail.com>
2015-09-06 01:11:16 +0300

    It compiles!
    Modified showcase usage from legacy branch to current latest. Needs testing.
    Added additional library to ensure image cropping woudl work on any device
    Promoted to API 16

---
+++ haxingModule/src/main/java/org/mots/haxsync/utilities/intents/IntentUtil.java
@@ -0,0 +1,29 @@
+package org.mots.haxsync.utilities.intents;
+
+
+import android.app.Activity;
+
+import java.util.List;
+
+/**
+ * Created by Tishun on 5.9.2015 г..
+ */
+public class IntentUtil {
+
+    public static IntentBuilder getIntentBuilder(Activity activity) {
+        throw new RuntimeException("Not yet developed");
+    }
+
+    public static IntentBuilder getFallbackBuilder() {
+        throw new RuntimeException("Not yet developed");
+    }
+
+    public static NameList getApps(Activity activity) {
+        throw new RuntimeException("Not yet developed");
+    }
+
+    public static class NameList {
+        public List<Character> namesAvail;
+        public List<Character> pkgsAvail;
+    }
+}
