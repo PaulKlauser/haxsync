ffe454e5c889d85f562fe264766678dee66269bc
tishun <tihomir.mateev@gmail.com>
2015-09-14 23:03:30 +0300

    Adding unit tests and coverage
    Dont know why I am even bothering with this but I really want to get it working

---
+++ haxingModule/src/test/java/org/codarama/haxsync/utilities/intents/IntentUtilTest.java
@@ -0,0 +1,24 @@
+package org.codarama.haxsync.utilities.intents;
+
+import android.app.Activity;
+
+import junit.framework.Assert;
+
+import org.codarama.haxsync.BuildConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.List;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class IntentUtilTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
